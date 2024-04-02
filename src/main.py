1b59e886784754356267812469a8fe27cefd4c03
Christian Le <crezle1999@gmail.com>
2024-04-02 15:00:55 +0200

    add comments to main file

--- src/main.py
+++ src/main.py
@@ -8,21 +8,26 @@ def main():
     img_path = 'data/apriltags/36h11/multiple_test18/0001.jpg'
     images_path = 'data/apriltags/36h11/multiple_test18'
     
+    # Find distortion coefficients
     camera.calibrate(board_size=(6, 9),
                      square_size=0.02,
                      calib_img_foldername=calibration_folder)
 
+    # Show calibration results
     camera.show_calibration_results(results_foldername=calibration_folder)
 
+    # Train apriltag parameters using images in same folder as subject image
     params = apriltag.train_parameters_apriltag(images_path=images_path,
                                                 num_corners=18,
                                                 save_params=True,
                                                 evals=150)
 
+    # Undistort subject image
     img = image_processing.undistort(img_path=img_path,
                                      calib_results_folder=calibration_folder,
                                      save_img=True)
 
+    # Detect apriltags in undistorted image
     apriltag.detect(img=img,
                     params=params)
 
